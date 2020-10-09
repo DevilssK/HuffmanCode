94883f329672c22707b2ba07c0c6dfac9fbb865f
Paul Simon <paul.simon@signature-code.com>
2020-10-09 16:40:19 +0200

    TreeBuilder

--- HuffmanCode/Program.cs
+++ HuffmanCode/Program.cs
@@ -7,6 +7,8 @@ namespace HuffmanCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ HuffmanCode/TreeBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCode
+{
+    class TreeBuilder
+    {
+        List<OccurenceItem> Occurences = new List<OccurenceItem>();
+
+        TreeBuilder()
+        {
+            Occurences.Add(new OccurenceItem()
+            {
+                Letter = 't',
+                Occurences = 1
+            });
+            Occurences.Add(new OccurenceItem()
+            {
+                Letter = 'a',
+                Occurences = 1
+            });
+            Occurences.Add(new OccurenceItem()
+            {
+                Letter = 's',
+                Occurences = 3
+            });
+            Occurences.Add(new OccurenceItem()
+            {
+                Letter = 'e',
+                Occurences = 2
+            });
+        }
+
+    }
+
+    public class OccurenceItem
+    {
+       public char Letter;
+       public int Occurences; 
+    }
+}
