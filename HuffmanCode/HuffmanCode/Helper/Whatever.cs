06fcc278e965c7f0148d79d1743c70afca5d5a5c
vladnelson <didimilca@hotmail.fr>
2020-10-16 11:33:03 +0200

    [Exos 2.4] - Encodage

---
+++ HuffmanCode/HuffmanCode/Helper/Whatever.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCode.HuffmanCode.Helper
+{
+    public class Whatever
+    {
+        public Whatever()
+        {
+
+        }
+        public Whatever(char _Value, string _BinHeffman)
+        {
+            this._value = _Value;
+            this._binHeffman = _BinHeffman;
+        }
+
+        private char _value;
+
+        public char Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+
+        private string _binHeffman;
+
+        public string BinHeffman
+        {
+            get { return _binHeffman; }
+            set { _binHeffman = value; }
+        }
+
+
+    }
+}
--- HuffmanCode/Program.cs
+++ HuffmanCode/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-﻿using HuffmanCode.Helper;
+using HuffmanCode.Helper;
 
 namespace HuffmanCode
 {
@@ -8,8 +10,6 @@ namespace HuffmanCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             Occurencies a = new Occurencies();
 
             var fileOccurencies = File.ReadAllText(Directory.GetCurrentDirectory() + "/TestOccurencies.txt");
@@ -30,8 +30,49 @@ namespace HuffmanCode
                 Console.WriteLine("Sauvegarder !!!");
             }
 
-            Console.WriteLine(fileBuilder.CounterChar($"{prexPath}Alice.txt"));
-            Console.WriteLine(fileBuilder.CounterChar($"{prexPath}Output.txt"));
+
+            Console.WriteLine($"Nombre de caractere dans le fichier Alice : {fileBuilder.CounterChar($"{prexPath}Alice.txt")}");
+            Console.WriteLine($"Nombre de caractere dans le fichier Output : {fileBuilder.CounterChar($"{prexPath}Output.txt")}");
+
+
+            Dictionary<char, string> ee = new Dictionary<char, string>();
+            ee.Add('A', "1");
+            ee.Add('a', "11");
+            ee.Add('e', "0");
+            ee.Add('i', "100");
+            ee.Add('o', "111");
+            ee.Add('u', "011");
+            ee.Add(' ', "101");
+
+
+
+            //******************************************************************************************************************
+            //  2.4 f - Écrire une fonction qui traduit un texte en une suite binaire basée sur un dictionnaire de Huffman.
+            //*******************************************************************************************************************
+            // Création du chronomètre.
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Démarrage du chronomètre.
+            stopwatch.Start();
+
+            Console.WriteLine(fileBuilder.TranslateContentToBinByHuffMan(ee, $"{prexPath}Alice.txt"));
+
+            stopwatch.Stop();
+            Console.WriteLine("Durée d'exécution 2.4 f : {0} sec.", stopwatch.Elapsed.TotalSeconds);
+
+            //*******************************************************************************************************************************************************************
+            //  2.4 g - Écrire une fonction qui compresse un fichier texte. Le fichier d’entrée ne sera pas modifié, un autre fichier, contenant le texte compressé sera créé.
+            //*******************************************************************************************************************************************************************
+            bool isCompresse = fileBuilder.FileCompresse($"{prexPath}Alice.txt", $"{prexPath}Output2.txt", ee);
+            if (isCompresse)
+            {
+                Console.WriteLine("Compresse Succes !!");
+            }
+            else
+            {
+                Console.WriteLine("Compresse Failed !!");
+            }
+
             Console.ReadLine();
         }
 
